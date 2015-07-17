dbde8167abc84a66e578580f30e0b47c6eca224e
Aurélien Bourdon <aurelien.bourdon@gmail.com>
2015-07-17 03:59:08 +0200

    feat(init): First stable version

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gravitee-policy-maven-archetype
 Maven Archetype to create a Gravitee Policy
+
+[![Build Status](http://build.gravitee.io/jenkins/buildStatus/icon?job=gravitee-policy-maven-archetype)](http://build.gravitee.io/jenkins/job/gravitee-policy-maven-archetype/)
+
+## Example
+mvn archetype:generate \
+    -DarchetypeGroupId=io.gravitee.maven.archetypes\
+    -DarchetypeArtifactId=gravitee-policy-maven-archetype\
+    -DarchetypeVersion=1.0.0-SNAPSHOT\
+    -DartifactId=my-policy\
+    -DgroupId=my.project.gravitee.policies\
+    -Dversion=1.0.0-SNAPSHOT\
+    -DpolicyName=MyPolicy
+
+More documentation will come...
---
+++ src/main/resources/archetype-resources/src/main/java/__policyName__.java
@@ -0,0 +1,79 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ${package};
+
+import io.gravitee.gateway.api.PolicyChain;
+import io.gravitee.gateway.api.Request;
+import io.gravitee.gateway.api.Response;
+import io.gravitee.gateway.api.policy.Policy;
+import io.gravitee.gateway.api.policy.annotations.OnRequest;
+import io.gravitee.gateway.api.policy.annotations.OnResponse;
+
+/**
+ * TODO put a detailed documentation on what is a Policy and how to define a new one
+ */
+@SuppressWarnings("unused")
+public class ${policyName} implements Policy {
+
+    /**
+     * The associated configuration to this ${policyName}
+     */
+    private ${policyName}Configuration configuration;
+
+    /**
+     * Create a new ${policyName} instance based on its associated configuration
+     *
+     * @param configuration the associated configuration to the new ${policyName} instance
+     */
+    public ${policyName}(${policyName}Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @OnRequest
+    @Override
+    public void onRequest(Request request, Response response, PolicyChain policyChain) {
+        // Add a dummy header
+        request.headers().put("X-DummyHeader", "Dummy header value");
+
+        // Finally continue chaining
+        policyChain.doNext(request, response);
+    }
+
+    @OnResponse
+    @Override
+    public void onResponse(Request request, Response response, PolicyChain policyChain) {
+        if (isASuccessfulResponse(response)) {
+            policyChain.doNext(request, response);
+        } else {
+            policyChain.doError(new RuntimeException("Not a successful response"));
+        }
+    }
+
+    private static boolean isASuccessfulResponse(Response response) {
+        switch (response.status() / 100) {
+            case 1:
+            case 2:
+            case 3:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+}
